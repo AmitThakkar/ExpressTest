96cc134074563fff9e89feccbddc27aeea00395c
Amit Kumar <amit.kumar@intelligrape.com>
2013-10-10 16:27:37 +0530

    Showing User list

--- public/js/user/controller.js
+++ public/js/user/controller.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('meanStack.user.controllers', [])
-    .controller('UserListController', [function () {
+    .controller('UserListController', ["$scope", "$http", function ($scope, $http) {
+        $http.get("/user/list")
+            .success(function (data, status) {
+                if (status == 200) {
+                    $scope.users = data;
+                } else {
+                    alert(data);
+                }
+            })
+            .error(function (data, status) {
+                console.log(data, status)
+            });
     }])
     .controller('UserCreateController', [function () {
     }]);
