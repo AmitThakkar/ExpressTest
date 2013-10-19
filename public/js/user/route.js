c601cc367ec706ff02692d7b6e321702031bfa49
Amit Kumar <amit.kumar@intelligrape.com>
2013-10-19 20:55:12 +0530

    Now route is also generating with scaffold module.

--- public/js/user/route.js
+++ public/js/user/route.js
@@ -1,8 +1,10 @@
+"use strict";
+
 function getUserRoute() {
     return [
-        {routeURL: '/user/list/:max/:offset', templateUrl: 'user/list.html', controller: 'UserListController'},
-        {routeURL: '/user/create', templateUrl: 'user/create.html', controller: 'UserCreateController'},
-        {routeURL: '/user/show/:id', templateUrl: 'user/show.html', controller: 'UserShowController'},
-        {routeURL: '/user/edit/:id', templateUrl: 'user/edit.html', controller: 'UserEditController'}
+        {routeURL: "/user/list/:max/:offset", templateUrl: "user/list.html", controller: "UserListController"},
+        {routeURL: "/user/create", templateUrl: "user/create.html", controller: "UserCreateController"},
+        {routeURL: "/user/show/:id", templateUrl: "user/show.html", controller: "UserShowController"},
+        {routeURL: "/user/edit/:id", templateUrl: "user/edit.html", controller: "UserEditController"}
     ];
 }
